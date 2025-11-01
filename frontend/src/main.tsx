410ddbbba35595243f628664bc580049c3010f78
cRohinic <150899608+cRohinic@users.noreply.github.com>
2025-11-01 12:35:27 +0530

    Fix ReactDOM and TS declaration errors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // optional but recommended
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-    <ToastContainer/>
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root') as HTMLElement;
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+      <ToastContainer />
+    </StrictMode>
+  );
+}
